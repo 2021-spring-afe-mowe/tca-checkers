0bdef895d36877fd4f98f7fb269922b693290c6a
pugDad26 <63824751+pugDad26@users.noreply.github.com>
2021-02-17 16:21:42 -0600

    created components for screens
    ng generate component "component name"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeScreenComponent } from './welcome-screen/welcome-screen.component';
+import { StatsScreenComponent } from './stats-screen/stats-screen.component';
+import { GameScreenComponent } from './game-screen/game-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeScreenComponent,
+    StatsScreenComponent,
+    GameScreenComponent
   ],
   imports: [
     BrowserModule,
