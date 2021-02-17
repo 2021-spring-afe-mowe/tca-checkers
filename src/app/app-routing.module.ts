0bd3c0556337b1bf220d11d1bfe3b2cc7efe9cc1
pugDad26 <63824751+pugDad26@users.noreply.github.com>
2021-02-17 16:34:55 -0600

    Set up routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Import the components
+import { WelcomeScreenComponent } from './welcome-screen/welcome-screen.component';
+import {StatsScreenComponent } from './stats-screen/stats-screen.component';
+import { GameScreenComponent } from './game-screen/game-screen.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+  { path: 'welcome', component: WelcomeScreenComponent },
+  { path: 'stats', component: StatsScreenComponent },
+  { path: 'game', component: GameScreenComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
