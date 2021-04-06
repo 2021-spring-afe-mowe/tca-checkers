e17cd6511286bfd8f60532b473404bf110763939
pugDad26 <63824751+pugDad26@users.noreply.github.com>
2021-04-05 20:02:17 -0500

    working on consolidating welcome and stats screens into one
    no longer using service for playerTwo variable.  will have to adjust logic for stats.

--- src/app/welcome-screen/welcome-screen.component.ts
+++ src/app/welcome-screen/welcome-screen.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { SharedDataService, Opponents } from '../shared-data.service';
+import { SharedDataService, Opponents, Games } from '../shared-data.service';
 
 @Component({
   selector: 'app-welcome-screen',
@@ -11,8 +11,20 @@ export class WelcomeScreenComponent implements OnInit {
 
   nameInTextInput = "";
 
-  clicked = false;
+  //For *ngIf
   toggleOn;
+
+  //variable for stats
+  checkerGames: Games[] = [];
+  groupByOpponnentName: any;
+  shapeForByPlayerStats: any;
+
+  //Variables for Game Results
+  numberOfWins: number;
+  numberOfLosses: number;
+  numberOfDraws: number;
+  numberOfGames: number;
+  numberOfQuits: number;
 
   //Everytime there is a change to opponents, called and updated on screen.
       //Tied to ngFor on welcome screen
@@ -24,8 +36,15 @@ export class WelcomeScreenComponent implements OnInit {
 
   constructor(private sharedDataSvc: SharedDataService) {}
 
+  //Variable For Opponent
+  // playerTwo = this.sharedDataSvc.getPlayerTwo();
+  playerTwo = "";
+
+  //Get the games array here to display on view
   ngOnInit() {
     // this.opponents = this.sharedDataSvc.getOpponents();
+    this.checkerGames = this.sharedDataSvc.getGamesData();
+    this.shapeData();
   }
 
   addNewOpponent() {
@@ -41,7 +60,45 @@ export class WelcomeScreenComponent implements OnInit {
 
   //set the value for chosen opponent
   selectPlayerTwo(playerTwo: string) {
-    this.sharedDataSvc.setPlayerTwo(playerTwo);
+    // this.sharedDataSvc.setPlayerTwo(playerTwo);
+    this.playerTwo = playerTwo;
     console.log(playerTwo);
+  }
+
+  //method for game stats
+  shapeData() {
+    // Group by...
+    const groupByOpponnentName = this.checkerGames.reduce(
+      (acc, x) => acc.set(x.opponentName, [...acc.get(x.opponentName) || [], x])
+        , new Map()
+    );
+
+    console.log([...groupByOpponnentName]);
+
+    const shapeForByPlayerStats = [...groupByOpponnentName].map(x => ({
+      opponentName: x[0]
+      , numberOfGames: x[1].length
+      , wins: x[1].filter(y => y.result == "W").length
+      , losses: x[1].filter(y => y.result == "L").length
+      , draws: x[1].filter(y => y.result == "D").length
+      , quits: x[1].filter(y => y.result == "Q").length
+    }));
+
+    console.log(shapeForByPlayerStats);
+
+    
+    //Use a filter on shapeForByPlayerStats to find opponent name 
+        //and get necessary opponent information??????
+    const resultsForGames = shapeForByPlayerStats.filter(x => x.opponentName == this.playerTwo);
+
+    //Logic for when a user views stats vs a new player not yet in the DB, don't want it
+        //To be blank
+
+    //Get number of wins, losses, and draws from games vs opponent
+    resultsForGames.filter(x => this.numberOfWins = x.wins);
+    resultsForGames.filter(x => this.numberOfLosses = x.losses);
+    resultsForGames.filter(x => this.numberOfDraws = x.draws);
+    resultsForGames.filter(x => this.numberOfQuits = x.quits);
+    resultsForGames.filter(x => this.numberOfGames = x.numberOfGames);
   }
 }
